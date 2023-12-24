def3e99a826e72e44b75c8267b2357e976845dc3
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2023-12-24 14:23:29 -0600

    Setup prettier and bootstrap theme
    - Use Vapor theme from bootswatch.
    - Setup prettier config file to maintain consistency in the project.
    - Remove unused files.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,13 @@ module.exports = {
 	settings: { react: { version: '18.2' } },
 	plugins: ['react-refresh'],
 	rules: {
-    'react-refresh/only-export-components': [
-		'warn',
-		{ allowConstantExport: true },
-    ],
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		// Add alphabetical imports rule
+		'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
+		// Add rule to enforce semicolons at the end of statements
+		'semi': ['error', 'always'],
 	},
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Card, Container } from 'react-bootstrap';
 
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	)
+		<Container>
+			<Card style={{ width: '18rem' }} className="bg-black border-primary mt-2">
+			<Card.Body>
+				<Card.Title>Card Title</Card.Title>
+				<Card.Text>
+				Some quick example text to build on the card title and make up the
+				bulk of the cars content.
+				</Card.Text>
+				<Button variant="primary">Go somewhere</Button>
+			</Card.Body>
+			</Card>
+		</Container>
+	);
 }
 
-export default App
+export default App;
